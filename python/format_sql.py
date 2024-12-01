c02753c3efa84436216643c569a7795dae4d2fab
Ole-Jakob Olsen <ole.jakob.olsen@ngi.no>
2024-12-01 01:31:18 +0100

    Reindent aligned

--- python/format_sql.py
+++ python/format_sql.py
@@ -8,10 +8,11 @@ result = sqlparse.format(
     keyword_case="upper",
     indentifier_case="lower",
     reindent=True,
-    # reindent_aligned=True,
+    reindent_aligned=True,
     indent_width=4,
     output_format="sql",
     wrap_after=80,
 )
+
 
 print(result.strip())
